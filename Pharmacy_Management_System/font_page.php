7d6e3f5b91e79f09e953dee6d030686a35febd94
ｓａｄｉｋ _ｉｑｂａｌ_ａｋｉｌ <69757433+sm-akil@users.noreply.github.com>
2025-01-18 22:42:21 +0600

    Add files via upload

---
+++ Pharmacy_Management_System/font_page.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/home1.css">
+	<title>Pharmacy_Management_System~home</title>
+</head>
+<body>
+<div class="hero">
+
+	<div class="navbar">
+		<img src="images/logo.png" class="logo">
+		<h2 class="txt1">『𝘀𝗺_𝗮𝗸𝗶𝗹•𝘄𝗲𝗯𝗭𝗼𝗻𝗲』</h2>
+           <button type="button" class="btn1">𝗩𝗜𝗘𝗪</button>
+	</div>
+
+    <div class="content">
+	<strong><h2>𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐨𝐮𝐫 </h2></strong>
+	<h1>ℙ𝕙𝕒𝕣𝕞𝕒𝕔𝕪 <br>𝕄𝕒𝕟𝕒𝕘𝕖𝕞𝕖𝕟𝕥 𝕊𝕪𝕤𝕥𝕖𝕞<br>ℙ𝕣𝕠𝕛𝕖𝕔𝕥</h1>
+
+
+	<!--<button type="button" class="btn2">ＯＶＥＲＶＩＥＷ</button>-->
+      <div class="btn">
+      <a href="http://localhost/Pharmacy-Management/index.php"><span>𝗹𝗼𝗴 𝗶𝗻 𝗵𝗲𝗿𝗲</span></a>
+      </div>
+
+
+    </div>
+  
+
+
+    <div class="side-br">
+    	<img src="images/menu.png" class="menu">
+
+           <div class="social-link">
+           	   <a href="https://www.facebook.com/profile.loaded/"><img src="images/fb.png"></a>
+           	   <a href="https://www.instagram.com/sm_akil/"><img src="images/ig.png"></a>
+           	   <a href="https://twitter.com/akil_sm9"><img src="images/tw.png"></a>
+           	   <a href="https://github.com/sm-akil"><img src="images/git.png"></a>
+
+           </div>
+
+             <div class="useful-link">
+           	   <a href="https://www.facebook.com/"><img src="images/share.png"></a>
+           	   <img src="images/info.png">
+           </div>
+      </div>
+      
+    <div class="bubbles">
+    	<img src="images/bubble.png">
+    	<img src="images/bubble.png">
+    	<img src="images/bubble.png">
+    	<img src="images/bubble.png">
+    	<img src="images/bubble.png">
+    	<img src="images/bubble.png">
+      <img src="images/bubble.png">
+    </div>
+
+ </div>
+</body>
+</html> 
---
+++ Pharmacy_Management_System/home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard - Home</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<script src="bootstrap/js/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="images/icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="css/sidenav.css">
+    <link rel="stylesheet" href="css/home.css">
+    <script src="js/restrict.js"></script>
+  </head>
+  <body>
+    <?php include "sections/sidenav.html"; ?>
+    <div class="container-fluid">
+              <div class="h5">Todays Report</div>
+              <table class="table table-bordered table-striped table-hover">
+                <tbody>
+                  <?php
+                    require 'php/db_connection.php';
+                    if($con) {
+                      $date = date('Y-m-d');
+                  ?>
+                  <tr>
+                    <?php
+                      $total = 0;
+                      $query = "SELECT NET_TOTAL FROM invoices WHERE INVOICE_DATE = '$date'";
+                      $result = mysqli_query($con, $query);
+
+                      while($row = mysqli_fetch_array($result))
+                        $total = $total + $row['NET_TOTAL'];
+                    ?>
+                    <th>Total Sales</th>
+                    <th class="text-success">Rs. <?php echo $total; ?></th>
+                  </tr>
+                  <tr>
+                    <?php
+                      //echo $date;
+                      $total = 0;
+                      $query = "SELECT TOTAL_AMOUNT FROM purchases WHERE PURCHASE_DATE = '$date'";
+                      $result = mysqli_query($con, $query);
+                      while($row = mysqli_fetch_array($result))
+                        $total = $total + $row['TOTAL_AMOUNT'];
+                    }
+                    ?>
+                    <th>Total Purchase</th>
+                    <th class="text-danger">Rs. <?php echo $total; ?></th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </div>
+
+        <hr style="border-top: 2px solid #ff5252;">
+
+        <div class="row">
+
+          <?php
+            function createSection2($icon, $location, $title) {
+              echo '
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3" style="padding: 10px;">
+              		<div class="dashboard-stats" style="padding: 30px 15px;" onclick="location.href=\''.$location.'\'">
+              			<div class="text-center">
+                      <span class="h1"><i class="fa fa-'.$icon.' p-2"></i></span>
+              				<div class="h5">'.$title.'</div>
+              			</div>
+              		</div>
+                </div>
+              ';
+            }
+            createSection2('address-card', 'new_invoice.php', 'Create New Invoice');
+            createSection2('handshake', 'add_customer.php', 'Add New Customer');
+            createSection2('shopping-bag', 'add_medicine.php', 'Add New Medicine');
+            createSection2('group', 'add_supplier.php', 'Add New Supplier');
+            createSection2('bar-chart', 'add_purchase.php', 'Add New Purchase');
+            createSection2('book', 'sales_report.php', 'Sales Report');
+            createSection2('book', 'purchase_report.php', 'Purchase Report');
+          ?>
+
+        </div>
+        <!-- form content end -->
+
+        <hr style="border-top: 2px solid #ff5252;">
+
+      </div>
+    </div>
+  </body>
+</html>
